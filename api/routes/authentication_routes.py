b1e4514b0b787a534345cf71964fb126028a27d1
Stephen 'Stephen' Rodrigues <stephen.r@COH000420.local>
2022-04-02 01:26:18 +0700

    auth, author & blog api routes created

---
+++ api/routes/authentication_routes.py
@@ -0,0 +1,36 @@
+"""Endpoints module."""
+from werkzeug.security import check_password_hash
+import jwt
+import datetime
+
+from fastapi import APIRouter, Depends, HTTPException
+from dependency_injector.wiring import inject, Provide
+from api.containers import Container
+from api.models.author import Author
+from api.services import AuthorService
+from api.views.login.login_view import LoginRequest
+
+
+authentication_route = APIRouter()
+
+
+"""Endpoint authenticate author & generate JWT Token"""
+@authentication_route.post("/authenticate")
+@inject
+async def authenticate(model: LoginRequest, author_service: AuthorService = Depends(Provide[Container.author_service]),):
+    author: Author = await author_service.get_author(model.email)
+    if not author:
+        raise HTTPException(status_code=401, detail="Invalid login credentials.")
+
+    if check_password_hash(author.hashed_password, model.password):
+        # generates the JWT Token
+        token = jwt.encode({
+                        'id': author.id,
+                        'name': author.name,
+                        'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes = 120)
+                    }, "secret_config", algorithm="HS256")
+        
+        return {'token' : token}
+
+    
+    raise HTTPException(status_code=401, detail="Invalid login credentials.")
